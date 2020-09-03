db5d274f3ce39a4c38f814c2fc18546606851a3c
Ian Ingram <ianishidden@gmail.com>
2020-09-03 15:46:14 -0700

    method to save images that aren't in a single excluded category (motivated by wanting everything but 'background') when classification score is over the threshold specified in config file.

--- vision.py
+++ vision.py
@@ -1,4 +1,6 @@
+import re
 import os
+from datetime import datetime
 
 import numpy as np
 import cv2
@@ -10,7 +12,7 @@ class ImageClassificationSystem():
 
     def __init__(self, configs):
         # CV constants
-        # self.CONF_THRESHOLD = configs['CONF_THRESHOLD']
+        self.CONF_THRESHOLD = configs['CONF_THRESHOLD']
 
         MODEL_PATH = configs['MODEL_PATH']
         MODEL = os.path.join(MODEL_PATH, configs['MODEL_CONFIG_FILE'])
@@ -20,21 +22,43 @@ class ImageClassificationSystem():
         # prepare neural network
         self.network = nn.ImageClassifierHandler(MODEL)
 
+        self.recorded_image_count = 0
+
     def infer(self, stream):
         # Construct a numpy array from the stream
         data = np.fromstring(stream.getvalue(), dtype=np.uint8)
         # "Decode" the image from the array, preserving color
-        frame = cv2.imdecode(data, 1)
+        self.frame = cv2.imdecode(data, 1)
 
-        self.result, inference_time = self.network.infer(frame)
+        self.result, inference_time = self.network.infer(self.frame)
 
+    def _extract_label_and_score(self):
+        label = self.CLASSES[self.result[0][0]]
+        score = self.result[0][1]
+
+        return label, score
+        
     def print_report(self):
         if len(self.result) > 0:
-            label = self.CLASSES[self.result[0][0]]
-            score = self.result[0][1]
+            label, score = self._extract_label_and_score()
             print('Top 1 label is {} with score: {}'.format(label, score))
         else:
             print('Inference resulted in no class label.')
+
+    def save_image_of_anything_but(self, excluded_class):
+        # also thresholds on score threshold defined in config file
+        if len(self.result) > 0:
+            label, score = self._extract_label_and_score()
+            if label != excluded_class and score >= self.CONF_THRESHOLD:
+                label = re.sub('[()]', '', label)
+                label = '_'.join(label.split(' '))
+                print(label)
+                now = datetime.now()
+                timestamp = now.strftime("%Y-%m-%dT%H-%M-%S.%f")[:-3]
+                filename = '{}_{}.jpeg'.format(timestamp, label)
+                self.recorded_image_count += 1
+                print('Saving image.')
+                cv2.imwrite(filename, self.frame)
             
 
 class ObjectDetectionSystem():
