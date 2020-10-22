bb19ff1c2051a086e207800d9f5bbe2c15a61787
Ian Ingram <ianishidden@gmail.com>
2020-10-22 14:10:42 -0700

    draws a box on preview (using picamera overlays) on highest ranked box from object detector.

--- objdetect_continuously.py
+++ objdetect_continuously.py
@@ -3,6 +3,7 @@ import argparse
 import yaml
 
 import picamera
+from PIL import Image, ImageDraw
 
 import vision
 
@@ -27,15 +28,61 @@ camera = picamera.PiCamera()
 camera.rotation = configs['CAMERA_ANGLE']
 camera.resolution = resolution
 
+
+
 if configs['PREVIEW_ON']:
     camera.start_preview()
 
+
+    overlay = Image.new('RGBA', resolution, (0, 0, 0, 0))
+
+    draw = ImageDraw.Draw(overlay)
+    draw.rectangle([(100, 100), (200, 200)], outline=(255,0,0), width=3)
+
+
+    pad = Image.new('RGBA',
+                    (((overlay.size[0] + 31) // 32) *32,
+                     ((overlay.size[1] + 15) // 16) * 16,
+                    ))
+
+    pad.paste(overlay, (0,0))
+
+    o = camera.add_overlay(pad.tobytes(), size=overlay.size)
+    o.alpha = 128
+    o.layer = 3
+
+
+
+    
 for _ in camera.capture_continuous(stream, format='jpeg'):
     stream.truncate()
     stream.seek(0)
 
     detector.infer(stream)
     detector.print_report()
+    top_box =  detector.top_box()
 
+    if top_box is not None:
+        left, top, width, height = top_box
+
+        camera.remove_overlay(o)
+        overlay = Image.new('RGBA', resolution, (0, 0, 0, 0))
+
+        draw = ImageDraw.Draw(overlay)
+        draw.rectangle([(left, top), (left+width, top + height)],
+                       outline=(255,0,0),
+                       width=3)
+
+
+        pad = Image.new('RGBA',
+                        (((overlay.size[0] + 31) // 32) *32,
+                         ((overlay.size[1] + 15) // 16) * 16,
+                        ))
+
+        pad.paste(overlay, (0,0))
+
+        o = camera.add_overlay(pad.tobytes(), size=overlay.size)
+        o.alpha = 128
+        o.layer = 3
 
 
