a6b7cb8de2441a079955cf4d1841d7fc666c7114
Ian Ingram <iingram@sandiegozoo.org>
2020-11-25 13:27:34 -0800

    - takes IP from command line
    - more clearly laid out

--- image_display_server.py
+++ image_display_server.py
@@ -1,28 +1,32 @@
-import io
+import sys
+import time
 import socket
 import struct
-import time
+import io
 
 import numpy as np
 import cv2
 
+ip = sys.argv[1]
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('192.168.1.66', 65432))
+server_socket.bind((ip, 65432))
 server_socket.listen()
 
 print('[INFO] Waiting for client connection.')
-connection = server_socket.accept()[0].makefile('rb')
-print('[INFO] Connection made.')
+connection, address = server_socket.accept()
+socket_stream = connection.makefile('rb')
+print('[INFO] Connection made to {}.'.format(address))
 
 try:
     while True:
         start = time.time()
         image_len = struct.unpack('<L',
-                                  connection.read(struct.calcsize('<L')))[0]
+                                  socket_stream.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
         image_stream = io.BytesIO()
-        image_stream.write(connection.read(image_len))
+        image_stream.write(socket_stream.read(image_len))
         image_stream.seek(0)
 
         data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
@@ -33,5 +37,5 @@ try:
         cv2.imshow('here', image)
         cv2.waitKey(1)
 finally:
-    connection.close()
+    socket_stream.close()
     server_socket.close()
