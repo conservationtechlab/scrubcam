a17ce6760bfe50eb1517ea2ded4887fc85a56ed8
Ian Ingram <iingram@sandiegozoo.org>
2020-12-01 17:26:10 -0800

    creating part of the mechanics for being able to send commands to the client with the first goal of being able to request an image be taken at any point.  everything is in place but keyboard input of a command (probably will want to get the socket communication in a thread for this.)

--- image_display_server.py
+++ image_display_server.py
@@ -8,34 +8,43 @@ import numpy as np
 import cv2
 
 ip = sys.argv[1]
+port = int(sys.argv[2])
 
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind((ip, 65432))
-server_socket.listen()
+command = 0
 
-print('[INFO] Waiting for client connection.')
-connection, address = server_socket.accept()
-socket_stream = connection.makefile('rb')
-print('[INFO] Connection made to {}.'.format(address))
+with socket.socket() as server_socket:
+    server_socket.bind((ip, port))
+    server_socket.listen()
 
-try:
+    print('[INFO] Waiting for client connection.')
+    connection, address = server_socket.accept()
+    socket_stream = connection.makefile('rwb')
+    print('[INFO] Connection made to {}.'.format(address))
+
     while True:
         start = time.time()
-        image_len = struct.unpack('<L',
-                                  socket_stream.read(struct.calcsize('<L')))[0]
+
+        socket_stream.write(struct.pack('<L', command))
+        socket_stream.flush()
+        
+        message = socket_stream.read(struct.calcsize('<L'))
+        image_len = struct.unpack('<L', message)[0]
+
         if not image_len:
             break
-        image_stream = io.BytesIO()
-        image_stream.write(socket_stream.read(image_len))
-        image_stream.seek(0)
 
-        data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(data, 1)
+        if image_len == 7:
+            pass
+        else:
+            image_stream = io.BytesIO()
+            image_stream.write(socket_stream.read(image_len))
+            image_stream.seek(0)
 
-        elapsed = time.time() - start
-        print('Got image. Took {} seconds. Displaying'.format(elapsed))
-        cv2.imshow('here', image)
-        cv2.waitKey(1)
-finally:
-    socket_stream.close()
-    server_socket.close()
+        
+            data = np.fromstring(image_stream.getvalue(), dtype=np.uint8)
+            image = cv2.imdecode(data, 1)
+
+            elapsed = time.time() - start
+            print('Got image. Took {} seconds. Displaying'.format(elapsed))
+            cv2.imshow('here', image)
+            cv2.waitKey(1)
