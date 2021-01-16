691ec06c7bad27c0a1cc975f7390e8fc1c536884
Ian Ingram <ianishidden@gmail.com>
2021-01-16 10:27:55 -0800

    pieces for sending labeled boxes along with image

--- networking.py
+++ networking.py
@@ -1,5 +1,6 @@
 import socket
 import struct
+import pickle
 
 import yaml
 
@@ -24,14 +25,37 @@ class ImageSocketHandler():
         self.connection.write(struct.pack('<L', 1))
         self.connection.flush()
 
+        self._send_image_data(stream)
+
+    def send_image_and_box(self, stream, box):
+        self.connection.write(struct.pack('<L', 2))
+        self.connection.flush()
+
+        self._send_box(box)
+        self._send_image_data(stream)
+
+    def _send_image_data(self, stream):
+        # send size of data in image stream
         stream.seek(0, 2)
         self.connection.write(struct.pack('<L', stream.tell()))
         self.connection.flush()
+        # send image data
         stream.seek(0)
         self.connection.write(stream.read())
+        self.connection.flush()
+
+    def _send_box(self, box):
+        data = pickle.dumps(box)
+        size = len(data)
+        self.connection.write(struct.pack('<L', size))
+        self.connection.flush()
+        self.connection.write(data)
+        self.connection.flush()
 
     def recv_command(self):
         message = self.connection.read(struct.calcsize('<L'))
+        if not message:
+            return None
         command = struct.unpack('<L', message)[0]
         return command
         
