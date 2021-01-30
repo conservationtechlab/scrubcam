f009cae6d170337e9d0ddb531f154cc663596106
Ian Ingram <ianishidden@gmail.com>
2021-01-30 15:58:53 -0800

    a setup.py for the package

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="scrubcam",
+    author="Ian Ingram",
+    version="0.0.1",
+    author_email="ianishidden@gmail.com",
+    description="Code for the Scrubcam and Sage Scrubcam projects.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
