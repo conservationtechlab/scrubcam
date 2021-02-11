205622ded98c5e6e091c0d375015c3ed408a7052
Ian Ingram <iingram@sandiegozoo.org>
2021-02-11 10:50:16 -0800

    now has a setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="scrubcam",
+    author="Ian Ingram",
+    version="0.0.1",
+    author_email="iingram@sandiegozoo.org",
+    description="Code for ScrubCam: Edge-AI-enabled field camera.",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
