71f75719334f34f8873afdcee7a28ee67ed59275
Ian Ingram <iingram@sandiegozoo.org>
2021-02-15 18:28:06 -0800

    now displays all recent captures in a grid so that user isn't only seeing most recent.

--- image_display_server.py
+++ image_display_server.py
@@ -4,6 +4,12 @@ import argparse
 import time
 
 import cv2
+import numpy as np
+import imutils
+
+from viztools.visualization import FlyingPicBox
+from viztools.visualization import init_pics, create_layout, GridDisplay
+from viztools import draw
 
 from scrubcam.display import Viewer
 from scrubcam.networking import ServerSocketHandler, create_image_dict
@@ -16,39 +22,90 @@ IP = args.ip
 PORT = int(args.port)
 
 logging.basicConfig(level=logging.INFO,
-                    format='[%(levelname)s] %(message)s')
-log = logging
+                    format='[%(levelname)s] %(message)s (%(name)s)')
+log = logging.getLogger('main')
+
+JUMP_SCREENS = False
+NUM_COLS = 10
+NUM_ROWS = 7
+COL_WIDTH = 330
+ROW_HEIGHT = 250
 
 
 def main():
+    flypics = []
+    layout = create_layout(NUM_ROWS,
+                           NUM_COLS,
+                           COL_WIDTH,
+                           ROW_HEIGHT,
+                           upside_down=False)
+
+    print('Grid: ' + str(NUM_COLS) + 'x' + str(NUM_ROWS))
     window = 'Viewer'
+    display = GridDisplay(window, JUMP_SCREENS, layout)
+    display_pics = init_pics(layout)
+    spot = 0
+    spot_count = 0
 
     image = create_image_dict()
     threads_stop = False
 
-    comms = ServerSocketHandler((IP, PORT), image, lambda : threads_stop)
+    comms = ServerSocketHandler((IP, PORT), image, lambda: threads_stop)
     comms.setDaemon(True)
     comms.start()
 
-    viewer = Viewer(image, lambda : threads_stop)
-    viewer.setDaemon(True)
-    viewer.start()
-    
+    # viewer = Viewer(image, lambda: threads_stop)
+    # viewer.setDaemon(True)
+    # viewer.start()
+
     try:
         while True:
+            display.refresh_canvas()
+
+            for flypic in flypics:
+                flypic.update()
+                flypic.display(display.canvas)
+            if len(flypics) > 10:
+                flypics = flypics[1:]
+
+            key = display.draw(display_pics)
+            if key == ord('q'):
+                threads_stop = True
+                break
+
             if image['img'] is not None:
-                cv2.imshow(window,
-                           image['img'])
-                key = cv2.waitKey(30)
-                if key == ord('q'):
-                    threads_stop = True
-                    break
+                if image['lboxes'] is not None:
+                    box = image['lboxes'][0]['box']
+                    image['img'] = draw.box_on_image(image['img'],
+                                                     box)
+ 
+                resized_image = imutils.resize(image['img'], width=320)
+                flypics.append(FlyingPicBox(resized_image,
+                                            np.array([layout[spot_count][0], 0]),
+                                            np.array(layout[spot_count])))
+                spot_count += 1
+                if spot_count >= len(layout):
+                    spot_count = 0
+
+                display_pics[spot].append(resized_image)
+                spot += 1
+                if spot >= len(layout):
+                    spot = 0
+
+                image['img'] = None
+
+                # cv2.imshow(window,
+                #            image['img'])
+                # key = cv2.waitKey(30)
+                # if key == ord('q'):
+                #     threads_stop = True
+                #     break
 
         cv2.destroyAllWindows()
 
     except KeyboardInterrupt:
         log.warning('Keyboard interrupt.')
-                
+
     threads_stop = True
     time.sleep(.1)
 
