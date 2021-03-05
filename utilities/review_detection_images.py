68c61ab4edbe86afa89ad23fec9e992370b07846
Ian Ingram <iingram@sandiegozoo.org>
2021-03-04 16:28:53 -0800

    now can toggle label filtering from cli
    also: only displays images in playback and video that pass the filter.

--- utilities/review_detection_images.py
+++ utilities/review_detection_images.py
@@ -12,7 +12,6 @@ import argparse
 import glob
 
 import cv2
-import numpy as np
 
 from viztools import draw
 
@@ -23,6 +22,10 @@ parser.add_argument('fps')
 parser.add_argument('conf')
 parser.add_argument('-x',
                     '--export',
+                    action='store_true',
+                    help='Export a video.')
+parser.add_argument('-f',
+                    '--filtering_on',
                     action='store_true',
                     help='Export a video.')
 parser.add_argument('-l',
@@ -36,14 +39,19 @@ export_video = args.export
 fps = int(args.fps)
 conf_threshold = float(args.conf)
 
-FILTER_LABELS = ['person']
+FILTER_LABELS = ['giraffe']
+if(args.filtering_on):
+    print('filtering on target labels is on')
+else:
+    print('filtering on target labels is off')
 
 img_paths = glob.glob(os.path.join(path, '*.jpeg'))
 img_paths = sorted(img_paths)
 
 images = []
+key = None
 for img_path in img_paths:
-    
+
     img = cv2.imread(img_path)
     img_shape = img.shape[:2][::-1]
 
@@ -58,17 +66,21 @@ for img_path in img_paths:
             bbox = [int(item) for item in row[2:]]
 
             caption = '{} {:.2f}'.format(label, score)
-            
-            if (score > conf_threshold
-                and label in FILTER_LABELS):
 
+            def meets_criteria(use_filter_labels):
+                if use_filter_labels:
+                    return score > conf_threshold and label in FILTER_LABELS
+                else:
+                    return score > conf_threshold
+
+            if (meets_criteria(args.filtering_on)):
                 if args.show_labels:
                     draw.labeled_box_on_image(img, bbox, caption)
                 draw.box_on_image(img, bbox, color=color)
-        
-    images.append(img)
-    cv2.imshow('View', img)
-    key = cv2.waitKey(msecs_per_image)
+
+                images.append(img)
+                cv2.imshow('View', img)
+                key = cv2.waitKey(msecs_per_image)
 
     if key == ord('q'):
         break
