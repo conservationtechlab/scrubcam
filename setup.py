78ebccbbb445736bf7e17218b7988df413b1bfbb
Ian Ingram <ianishidden@gmail.com>
2021-03-17 15:34:32 -0700

    adding known dependencies

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ import setuptools
 
 setuptools.setup(
     name="scrubcam",
-    author="Ian Ingram",
-    version="0.0.1",
-    author_email="iingram@sandiegozoo.org",
+    author="Conservation Technology Lab at the San Diego Zoo Wildlife Alliance",
     description="Code for ScrubCam: Edge-AI-enabled field camera.",
     packages=setuptools.find_packages(),
+    install_requires=[
+        'imutils',
+        'screeninfo',
+        'pillow',
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
     ],
 )
