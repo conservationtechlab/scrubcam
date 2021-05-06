1723ec6b07170fb26e7686ff315c0b28326f34a6
Ian Ingram <iingram@sandiegozoo.org>
2021-05-05 20:54:41 -0700

    fleshing out setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,29 @@
-import setuptools
+import os
+from setuptools import setup
 
+
+def read(filename):
+  path = os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
+  with open(path , 'r') as f:
+    return f.read()
+
+
+def find_version(text):
+  match = re.search(r"^__version__\s*=\s*['\"](.*)['\"]\s*$", text,
+                    re.MULTILINE)
+  return match.group(1)
+
+
+AUTHOR = "Conservation Technology Lab at the San Diego Zoo Wildlife Alliance"
+DESC = "Code for ScrubCam: Edge-AI-enabled wildlife field camera."
 
 setuptools.setup(
     name="scrubcam",
-    author="Conservation Technology Lab at the San Diego Zoo Wildlife Alliance",
-    description="Code for ScrubCam: Edge-AI-enabled wildlife field camera.",
+    description=DESC,
+    long_description=read('README.md')
+    license="MIT",
+    version=find_version(read('scrubcam/__init__.py'),
+    author=AUTHOR,
     packages=setuptools.find_packages(),
     install_required=[
         'pyyaml',
@@ -15,10 +34,12 @@ setuptools.setup(
         'opencv-python',
         'imutils',
         'pillow',
-        
+        'dencam',
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
+        'Development Status :: 2 - Pre-Alpha',
+        'Topic :: Scientific/Engineering',
     ],
 )
