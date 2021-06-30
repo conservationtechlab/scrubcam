17ab6f01f2aee06a464e7d3d942daae48402c35c
Ian Ingram <iingram@sandiegozoo.org>
2021-06-30 15:20:57 -0700

    Add utility to test scrubcam/scrubhub without graphics
    In situations where we are missing a screen or a display-related
    dependency, we sometimes need to test that scrubcam can connect to
    scrubhub.  This utility fills this need for now.  One weakness of this
    method is that any changes to the real scrubhub will need to be
    manually added to this utility but the need was pressing enough now to
    do this this way for the time being.

---
+++ utilities/mock_scrubhub.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+""" Dashboard for viewing images coming in over network from a scrubcam.
+
+"""
+import logging
+import argparse
+import time
+
+import cv2
+import numpy as np
+
+from scrubcam.networking import ServerSocketHandler, create_image_dict
+
+parser = argparse.ArgumentParser()
+parser.add_argument('ip')
+parser.add_argument('port')
+args = parser.parse_args()
+IP = args.ip
+PORT = int(args.port)
+
+logging.basicConfig(level=logging.INFO,
+                    format='[%(levelname)s] %(message)s (%(name)s)')
+log = logging.getLogger('main')
+
+JUMP_SCREENS = False
+NUM_COLS = 4
+NUM_ROWS = 3
+IMAGE_WIDTH = 700
+COL_WIDTH = IMAGE_WIDTH + 10
+ROW_HEIGHT = int(IMAGE_WIDTH * 9/16 + 10)
+
+CONF_THRESHOLD = .35
+
+
+def main():
+    print('Grid: ' + str(NUM_COLS) + 'x' + str(NUM_ROWS))
+    spot = 0
+
+    image = create_image_dict()
+    threads_stop = False
+
+    comms = ServerSocketHandler((IP, PORT), image, lambda: threads_stop)
+    comms.setDaemon(True)
+    comms.start()
+
+    # viewer = Viewer(image, lambda: threads_stop)
+    # viewer.setDaemon(True)
+    # viewer.start()
+
+    spots = {'giraffe': 0,
+             'person': 1,
+             'cat': 2,
+             'elephant': 3,
+             'zebra': 4,
+             'horse': 5,
+             'dog': 6,
+             'bird': 7,
+             'cow': 8,
+             'sheep': 9,
+             'bear': 10}
+
+    inverted = {v: k for k, v in spots.items()}
+    for spot in range(len(spots)):
+        blank = 255 * np.ones((int(IMAGE_WIDTH * 9/16), IMAGE_WIDTH, 3))
+        holder = cv2.putText(blank,
+                             inverted[spot],
+                             (50, 50),
+                             cv2.FONT_HERSHEY_SIMPLEX,
+                             1,
+                             (0, 0, 0),
+                             2,
+                             cv2.LINE_AA)
+
+    key = None
+    try:
+        while True:
+            # need to implement a CLI read of key since this used to
+            # be handle via CV GUI run from viztools elements
+            if key == ord('q'):
+                threads_stop = True
+                break
+
+            if image['img'] is not None:
+                if image['lboxes'] is not None:
+
+                    for lbox in image['lboxes']:
+                        confidence = lbox['confidence']
+                        if confidence > CONF_THRESHOLD:
+                            box = lbox['box']
+                            label = '{} {:.2f}'
+                            label = label.format(lbox['class_name'],
+                                                 confidence)
+
+                for lbox in image['lboxes']:
+                    if lbox['class_name'] in spots.keys():
+                        top_label = lbox['class_name']
+                        break
+
+                image['img'] = cv2.putText(image['img'],
+                                           top_label,
+                                           (50, 90),
+                                           cv2.FONT_HERSHEY_SIMPLEX,
+                                           3,
+                                           (255, 255, 255),
+                                           4,
+                                           cv2.LINE_AA)
+
+                spot = spots[top_label]
+
+                image['img'] = None
+
+        cv2.destroyAllWindows()
+
+    except KeyboardInterrupt:
+        log.warning('Keyboard interrupt.')
+
+    threads_stop = True
+    time.sleep(.1)
+
+
+if __name__ == "__main__":
+    main()
