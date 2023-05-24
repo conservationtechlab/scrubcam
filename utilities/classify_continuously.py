4381df869c7206d7b741f27019985a4f9b5f3e26
Ian Ingram <iingram@sandiegozoo.org>
2023-05-24 16:38:23 -0700

    Update this tool to work with current setup

--- utilities/classify_continuously.py
+++ utilities/classify_continuously.py
@@ -1,12 +1,19 @@
-# grabs stills, inferring on each, going as fast as it can
+"""Runs image classifier on each , inferring on each, going as fast as it can
 
+
+"""
+import logging
 import io
 import argparse
 import yaml
 
 import picamera
 
-import vision
+from scrubcam import vision
+
+logging.basicConfig(level='INFO',
+                    format='[%(levelname)s] %(message)s (%(name)s)')
+log = logging.getLogger('main')
 
 parser = argparse.ArgumentParser()
 parser.add_argument('config',
@@ -14,17 +21,17 @@ parser.add_argument('config',
 args = parser.parse_args()
 CONFIG_FILE = args.config
 
-with open(CONFIG_FILE) as f:
+with open(CONFIG_FILE, 'r', encoding='utf-8') as f:
     configs = yaml.load(f, Loader=yaml.SafeLoader)
 
 RECORD = configs['RECORD']
-    
+
 classifier = vision.ImageClassificationSystem(configs)
 
 stream = io.BytesIO()
 
 camera = picamera.PiCamera()
-camera.rotation = configs['CAMERA_ANGLE']
+camera.rotation = configs['CAMERA_ROTATION']
 if configs['PREVIEW_ON']:
     camera.start_preview()
 
